c79d8c91780cddd0d0cee7c292778cd3e58987a3
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-10-21 11:59:15 -0300

    Atualizando extensões ESLint e Prettier

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
+import { App } from './app.jsx'
+import './index.css'
 
-const rootElement = document.querySelector('.root')
+const rootElement = document.querySelector('[data-js="root"]')
 const root = createRoot(rootElement)
 
 root.render(
